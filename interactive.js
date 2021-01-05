fd5ad119840a1baa8c184ef85ccee41f84021306
Alex Cortez <acim650@gmail.com>
2021-01-04 17:19:13 -0800

    interactive.js

--- interactive.js
+++ interactive.js
@@ -1,16 +1,6 @@
 
-const btn = document.querySelector('.topnav');
-let menuOpen = false;
-btn.addEventListener('click', () => {
-    if (!menuOpen) {
-        btn.classList.add('open');
-        menuOpen = true;
-    } else {
-        btn.classList.remove('open');
-        menuOpen = false;
-    }
-});
+const menu = document.getElementById('sidebar');
 
-
-
-
+function show() {
+    menu.classList.toggle('active');
+}
