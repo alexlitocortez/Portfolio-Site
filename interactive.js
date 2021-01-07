bcc48d0e94af513e74e2d7aaf8540342ce90e18a
Alex Cortez <acim650@gmail.com>
2021-01-07 15:01:39 -0800

    interactive.js

--- interactive.js
+++ interactive.js
@@ -1,6 +1,26 @@
 
 const menu = document.getElementById('sidebar');
+const toggle = document.getElementById('toggle-btn');
+const transform = document.querySelector('span');
+const mq = window.matchMedia( "(min-width: 300px)" );
+const mz = window.matchMedia( "(max-width: 767px)" );
 
-function show() {
-    menu.classList.toggle('active');
-}
+toggle.addEventListener('click', () => {
+    if(mq.matches && mz.matches) {
+        menu.classList.toggle('active');
+    } else {
+        menu.classList.add('noAnimation');
+    }
+})
+
+
+
+
+
+
+
+
+// How to make toggle button stay an X only when the menu is out
+
+// 1. initialize screen sizes with variables
+// 2. use those variables to do if statements and use .remove for the .noAnimation class I made
