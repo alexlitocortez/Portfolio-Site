92aa7a69a577e1311e94ab4fb0811da4d9f713da
Alex Cortez <acim650@gmail.com>
2021-01-12 15:04:43 -0800

    interactive.js

--- interactive.js
+++ interactive.js
@@ -1,13 +1,24 @@
 
-const menu = document.getElementById('menu-two')
-const btn = document.getElementById('menu-btn');
-
+const btn = document.getElementById('toggle-btn');
+const menu = document.getElementById('menu-two');
+const spanOne = document.getElementById('line top');
+const spanTwo = document.getElementById('line middle');
+const spanThree = document.getElementById('line bottom');
 
 btn.addEventListener('click', () => {
     menu.classList.toggle('active');
+    click();
 })
 
+function click() {
+    spanOne.style.transform = "rotate(45deg)";
+    spanTwo.style.transform = "translateX(500px)";
+    spanThree.style.transform = "rotate(-45deg)";
+}
 
 
+
+// 1. Create an if/else function for when button is clicked or not
+// 2. Insert it into the addevent listener function
 
 
