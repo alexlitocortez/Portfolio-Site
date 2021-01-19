6065a76e7ed9622b01d2068e1a91d863d80b94dc
Alex Cortez <acim650@gmail.com>
2021-01-19 13:32:37 -0800

    interactive.js

--- interactive.js
+++ interactive.js
@@ -1,24 +1,45 @@
 
+const body = document.querySelector('body');
 const btn = document.getElementById('toggle-btn');
 const menu = document.getElementById('menu-two');
-const spanOne = document.getElementById('line top');
-const spanTwo = document.getElementById('line middle');
-const spanThree = document.getElementById('line bottom');
+const spanOne = document.getElementById('line-top');
+const spanTwo = document.getElementById('line-middle');
+const spanThree = document.getElementById('line-bottom');
+const fName = document.getElementById('fname');
+const icon = document.querySelector('li');
+const fa = document.getElementById('fa');
+
+
 
 btn.addEventListener('click', () => {
     menu.classList.toggle('active');
-    click();
+    spanOne.classList.toggle('foo');
+    spanTwo.classList.toggle('foo');
+    spanThree.classList.toggle('foo');
 })
 
-function click() {
-    spanOne.style.transform = "rotate(45deg)";
-    spanTwo.style.transform = "translateX(500px)";
-    spanThree.style.transform = "rotate(-45deg)";
-}
-
-
-
-// 1. Create an if/else function for when button is clicked or not
-// 2. Insert it into the addevent listener function
+// below listed default settings
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    
+  
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  
+  });
 
 
